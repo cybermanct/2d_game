aa25067d72c91eee92aa60f8c4b0ea4195b39ac3
cybermanct <serwillow.wolfe@gmail.com>
2023-01-04 02:16:38 +0300

    12314

--- Menu.gd
+++ Menu.gd
@@ -1,12 +1,7 @@
 extends Control
 
-func _ready():
-	pass # Replace with function body.
-
-
 func _on_Start_pressed():
 	get_tree().change_scene("res://Node2D.tscn")
-
 
 func _on_Quit_pressed():
 	get_tree().quit()
