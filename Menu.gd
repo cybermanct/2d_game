63a8bf90fc1197fc4dc03086b18bae28bc75e9c9
cybermanct <serwillow.wolfe@gmail.com>
2023-01-05 17:00:26 +0300

    Добавлено нормальное меню, с возможностью сделать туда шейдер

--- Menu.gd
+++ Menu.gd
@@ -1,10 +1,35 @@
-extends Control
+extends Node2D
 
-func _on_Start_pressed():
-	get_tree().change_scene("res://Node2D.tscn")
+var selected_menu = 0
 
-func _on_Quit_pressed():
-	get_tree().quit()
+func change_menu_color():
+	$NewGame.color = Color.webgray
+	$QuitGame.color = Color.webgray
+	
+	match selected_menu:
+		0:
+			$NewGame.color = Color.gray
+		1:
+			$QuitGame.color = Color.gray
 
-#хуй пизда
-#чё нажыфаывпвыап
+func _ready():
+	change_menu_color()
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_down"):
+		selected_menu = (selected_menu + 1) % 2;
+		change_menu_color()
+	elif Input.is_action_just_pressed("ui_up"):
+		if selected_menu > 0:
+			selected_menu = selected_menu - 1
+		else:
+			selected_menu = 2
+		change_menu_color()
+	elif Input.is_action_just_pressed("ui_enter"):
+		match selected_menu:
+			0:
+				# New game
+				get_tree().change_scene("res://Node2D.tscn")
+			1:
+				# Quit game
+				get_tree().quit()
