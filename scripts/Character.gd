50afb1e21fa5a118610d3e3ed2953dfdc03f3ced
cybermanct <serwillow.wolfe@gmail.com>
2023-01-07 00:51:11 +0300

    Создал иерархию и структуру проекта

---
+++ scripts/Character.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+export (int) var speed = 2000
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+		$AnimatedSprite.play("walk")
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+		$AnimatedSprite.flip_h = 1
+		$AnimatedSprite.play("walk")
+	velocity = velocity.normalized() * speed
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	$AnimatedSprite.flip_h = 0
+	get_input()
+	velocity = move_and_slide(velocity)
+	if velocity.x == 0:
+		$AnimatedSprite.play("default")
---
+++ scripts/Menu.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var selected_menu = 0
+
+func change_menu_color():
+	$NewGame.color = Color.webgray
+	$QuitGame.color = Color.webgray
+	
+	match selected_menu:
+		0:
+			$NewGame.color = Color.gray
+		1:
+			$QuitGame.color = Color.gray
+
+func _ready():
+	change_menu_color()
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_down"):
+		selected_menu = (selected_menu + 1) % 2;
+		change_menu_color()
+	elif Input.is_action_just_pressed("ui_up"):
+		if selected_menu > 0:
+			selected_menu = selected_menu - 1
+		else:
+			selected_menu = 1
+		change_menu_color()
+	elif Input.is_action_just_pressed("ui_enter"):
+		match selected_menu:
+			0:
+				# New game
+				get_tree().change_scene("res://scenes/Node2D.tscn")
+			1:
+				# Quit game
+				get_tree().quit()
