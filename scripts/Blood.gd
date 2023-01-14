fc979f3ddc1209970c904197ea9388229ab35621
bl4ckFrisk <glofeed9@gmail.com>
2023-01-14 18:57:14 +0300

    добавил ост и исправил наложение красной текстуры на шейдер

--- scripts/Blood.gd
+++ scripts/Blood.gd
@@ -1,7 +1,9 @@
 extends Sprite
 
+
 func _on_blood_trigger_body_entered(_body):
 	show()
-	$"../ost".pitch_scale = 0.4
+	$"../ost".stop()
+	$"../ost2".play()
 	$"../KinematicBody2D".speed = 100
 	#$"../Blood/blood_wall/CollisionShape2D".disabled = 1
